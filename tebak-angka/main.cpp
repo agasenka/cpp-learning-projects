f6568d455721fb3ee625f3d357c925c22ad7c021
agasenka <agasptama@gmail.com>
2026-07-08 16:20:40 +0700

    proyek tebak angka

---
+++ tebak-angka/main.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <limits>
+
+int main() {
+  srand(time(0));
+
+  int angkaAcak = rand() % 100 + 1;
+  int tebakan;
+  int totalTebakan = 0;
+
+  std::cout << "Tebak Angka\n";
+
+  std::cout << "Komputer telah memilih angka!\n";
+  do {
+    std::cout << "Tebak angka: ";
+
+    if (!(std::cin >> tebakan)) {
+      std::cout << "Input harus berupa angka!\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      continue;
+    }
+
+    if (tebakan < 1 || tebakan > 100) {
+      std::cout << "Masukkan angka hanya dari angka 1 sampai 100!\n";
+
+      continue;
+    }
+
+    totalTebakan++;
+
+    if (tebakan == angkaAcak) {
+      std::cout << "Angka berhasil ditebak!\n";
+    } else if (tebakan < angkaAcak) {
+      std::cout << "Tebakan kurang dari angka rahasia!\n";
+    } else {
+      std::cout << "Tebakan lebih dari angka rahasia!\n";
+    }
+
+  } while (tebakan != angkaAcak);
+
+  std::cout << "Kamu menebak sebanyak " << totalTebakan << "x!";
+}
